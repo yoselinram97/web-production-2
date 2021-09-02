cc4cbdbec74380fcdfd63d4b0ac88eafb53eef71
Yoselin <yoselinramirez13@hotmail.com>
2021-09-01 21:24:59 -0400

    again

--- script.js
+++ script.js
@@ -19,25 +19,3 @@ function showSlides() {
 }
 
 //this is for the video
-
-
-var myVideo = document.getElementById("video1");
-
-function playPause() {
-  if (myVideo.paused)
-    myVideo.play();
-  else
-    myVideo.pause();
-}
-
-function makeBig() {
-    myVideo.width = 560;
-}
-
-function makeSmall() {
-    myVideo.width = 320;
-}
-
-function makeNormal() {
-    myVideo.width = 420;
-}
