292c442d9d7bc19aa815e356009002d129372061
Yoselin <yoselinramirez13@hotmail.com>
2021-10-13 00:13:42 -0400

    adding week 6 work

---
+++ week6/week6.js
@@ -0,0 +1,33 @@
+//onload function
+$(function() {
+  $("#get-input").click(function() {
+    let textInput = $("#text-input").val()
+    //will reset if failed
+    $("#fail").text('')
+//adding our api
+    if (textInput !== '')
+    $.getJSON(`https://api.tvmaze.com/search/shows?q=${textInput}`, function(data) {
+          console.log('data: ', data)
+          if (data.length == 0) {
+                $("#fail").text("We couldn't find anything for that topic")
+              } else {
+                for (let i = 0; i < 5; i++) {
+                  let work = data[i]
+                  //here we are adding the title and summary of the shows
+                  $("#recommendations").append('<li>' + ' Title: ' + work.show.name + ' Language: ' + work.show.language +  ' Summary: ' + work.show.summary)
+                  //an image will also be added
+                  let img = $("<img/>", { src: work.show.image.medium })
+                  img.appendTo('#recommendations')
+                }
+              }
+
+            }).fail(function() {
+              $('#fail').text("We couldn't find anything for that topic.")
+              console.log("we can't find anything")
+            })
+
+
+    //reset the input
+    $('#text-input').val('')
+  })
+})
